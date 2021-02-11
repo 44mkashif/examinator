5522ad6bcb7014e31d6c43c2ecd5377a94d2afac
2017042 <u2017042@giki.edu.pk>
2021-02-11 05:54:27 +0500

    homepage css

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,20 @@ import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
 import { Link } from 'react-router-dom';
 import bgImage from './assets/cover.jpg';
+import logo from './assets/examinator-logo.jpeg';
+import 'fontsource-roboto';
+import Avatar from '@material-ui/core/Avatar';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     height: '100vh',
     backgroundImage: `url(${bgImage})`,
+    paddingTop: '70px',
+    backgroundSize: '100% 100%'
+  },
+  logo: {
+    height: '190px',
+    width: '190px'
   },
   paper: {
     margin: theme.spacing(8, 4),
@@ -19,11 +28,21 @@ const useStyles = makeStyles((theme) => ({
     alignItems: 'center',
     height: '30vh'
   },
+  avatar: {
+    margin: theme.spacing(1),
+    height: '190px',
+    width: '190px',
+    backgroundColor: theme.palette.secondary.main,
+  },
   button: {
     margin: theme.spacing(5, 3, 5),
-    height: '10vh',
-    width: '20vh'
+    height: '5vh',
+    width: '20vh',
+    borderWidth: '3px', 
+    boxShadow: '1px 2px 4px'
+    // borderRadius: '10%'
   },
+
   text: {
     margin: theme.spacing(0, 0, 5),
   }
@@ -35,18 +54,23 @@ function App() {
 
   return (
     <div className="App">
-      <Grid container alignItems="center" justify="left" component="main" className={classes.root}>
+      <Grid container justify="left" component="main" className={classes.root}>
         <CssBaseline />
         <Grid item elevation={6}>
           <div className={classes.paper}>
             <Grid container justify="center" className={classes.text}>
-              <Typography component="h1" variant="h2">
-                Examinator
+              <Avatar className={classes.avatar}>
+                <img className={classes.logo} src={logo} alt=""/>
+              </Avatar>
+            </Grid>
+            <Grid container justify="center" className={classes.text}>
+              <Typography className="intro" color="primary" variant='h3'>
+                EXAMINATOR
               </Typography>
             </Grid>
             <Grid container justify="center" >
               <Button
-                variant="contained"
+                variant="outlined"
                 color="primary"
                 className={classes.button}
                 component={Link} to="/student"
@@ -54,7 +78,7 @@ function App() {
                 Student Portal
               </Button>
               <Button
-                variant="contained"
+                variant="outlined"
                 color="primary"
                 className={classes.button}
                 component={Link} to="/teacher"
@@ -62,7 +86,7 @@ function App() {
                 Teacher Portal
               </Button>
               <Button
-                variant="contained"
+                variant="outlined"
                 color="primary"
                 className={classes.button}
                 component={Link} to="/admin/login"

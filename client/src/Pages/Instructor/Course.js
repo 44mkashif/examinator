900b62a43871338400ce82c04edf38a6844e2939
aliannus2 <ali.annus02@gmail.com>
2021-02-22 04:00:19 +0500

    Instructor Exam Page Added

--- client/src/Pages/Instructor/Course.js
+++ client/src/Pages/Instructor/Course.js
@@ -2,18 +2,14 @@ import React from 'react';
 
 import AppBar from './Components/AppBar';
 import ScheduleButton from './ScheduleButton';
-import PaperButton from './PaperButton';
 
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
 import Container from '@material-ui/core/Container';
 
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 
 import ButtonBase from '@material-ui/core/ButtonBase'; 
@@ -52,7 +48,7 @@ export default function Course() {
 
                             <Grid item key={exam} xs={12} sm={6} md={4}>
                                 <ButtonBase
-                                    onClick={event => { navigateTo('../instructor/course?id=1') }}
+                                    onClick={event => { navigateTo('../instructor/course/exam') }}
                                 >
                                     <Card className={classes.card}>
                                         <CardContent className={classes.cardContent}>
@@ -75,8 +71,9 @@ export default function Course() {
                                 </ButtonBase>
                             </Grid>
                         ))}
-                        <ScheduleButton />
+                        
                     </Grid>
+                    <ScheduleButton />
                 </Container>
             </div>
 

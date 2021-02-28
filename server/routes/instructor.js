14f5fcbc01c360b388cb0ce77a46436897029166
Muhammad Kashif <44mkashif@gmail.com>
2021-03-01 02:23:01 +0500

    Bugs fixed

--- server/routes/instructor.js
+++ server/routes/instructor.js
@@ -2,13 +2,13 @@
 
 const express = require('express');
 const { StudentController } = require('../controllers/StudentController');
-const { ExamController } = require('../controllers/ExamController');
+// const { ExamController } = require('../controllers/ExamController');
 
 const router = express.Router();
 
 router.post('/addStudent', StudentController.addStudent);
 router.delete('/deleteStudent', StudentController.deleteStudent);
 
-router.post('/addExam', ExamController.addExam);
+// router.post('/addExam', ExamController.addExam);
 
 module.exports = router

73e3183bcbb6134841b77bb2edd4b0331bd0ea6c
Saif Hashmi <saifhashmi008@gmail.com>
2021-02-28 08:33:43 +0500

    authentication-2

--- server/config/database.js
+++ server/config/database.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const connect = () => {
     // 'mongodb://localhost:27017/examinator'
-    const url = 'mongodb+srv://saifhashmi:Emn3Ae09No8jFglb@cluster0.pj1dt.mongodb.net/Examinator?retryWrites=true&w=majority';
+    // 'mongodb+srv://saifhashmi:Emn3Ae09No8jFglb@cluster0.pj1dt.mongodb.net/Examinator?retryWrites=true&w=majority'
+    const url = 'mongodb://localhost:27017/examinator';
     mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
     var db = mongoose.connection;
     db.on('error', err => {

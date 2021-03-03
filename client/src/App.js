09e5e23835064993bdf502ce7cfc6c0dd920691e
ahmed267115 <ahmedpervaiz302@gmail.com>
2021-03-04 03:06:59 +0500

    Fixed result page

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
-import './App.css';
 import { Link } from 'react-router-dom';
 import bgImage from './assets/cover.png';
 import logo from './assets/logo-dark.png';
@@ -18,7 +17,7 @@ const useStyles = makeStyles((theme) => ({
   },
   leftContainer: {
     height: '100vh',
-    // marginTop: '50vh'
+    
   },
   rightContainer: {
     height: '100vh'

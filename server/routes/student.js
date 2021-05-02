6a1c7c2b020c9ca587a3f94a1fe0c596295bb227
Saif Hashmi <saifhashmi008@gmail.com>
2021-05-02 20:27:59 +0500

    student exam get APIs added

--- server/routes/student.js
+++ server/routes/student.js
@@ -2,11 +2,16 @@
 
 const express = require('express');
 const { CourseController } = require('../controllers/CourseController');
+const { ExamController } = require('../controllers/ExamController');
 const { StudentController } = require('../controllers/StudentController');
 
 const router = express.Router();
 
 router.get('/courses', CourseController.getCourses);
+
+router.get('/exam', ExamController.getExam);
+router.get('/exams', ExamController.getExams);
+
 router.get('/get-image', StudentController.getStudentImage);
 
 module.exports = router

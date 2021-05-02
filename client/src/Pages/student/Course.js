9a843a2d1d435b4b1c1e19c9c6f01e3af8dd4140
Muhammad Kashif <44mkashif@gmail.com>
2021-05-02 18:00:54 +0500

    Integrated get exam api

--- client/src/Pages/student/Course.js
+++ client/src/Pages/student/Course.js
@@ -119,7 +119,7 @@ export default function Course() {
 
     CourseService.getCourse(courseId, authToken, false).then((courseFromDb) => {
         console.log(courseFromDb);
-        course = courseFromDb; 
+        course = courseFromDb;
     });
 
     ExamService.getExams(courseId, authToken).then((examsFromDb) => {
@@ -132,9 +132,22 @@ export default function Course() {
         })
 
         for (var i = 0; i < examData.length; i++) {
-            s[i] = (examData[i].startTime).toString();
-            d[i] = s[i].substring(0, 10);
-            t[i] = s[i].substring(11, 19);
+            const date = new Date(examData[i].startTime);
+
+            var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+            var hours = date.getHours();
+            var minutes = date.getMinutes();
+            var ampm = hours >= 12 ? 'PM' : 'AM';
+            hours = hours % 12;
+            hours = hours ? hours : 12; // the hour '0' should be '12'
+            minutes = minutes < 10 ? '0' + minutes : minutes;
+            var strTime = hours + ':' + minutes + ' ' + ampm;
+
+            console.log(strTime);
+
+            d[i] = months[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
+            t[i] = strTime;
         }
 
         setLoading(true);
@@ -195,7 +208,7 @@ export default function Course() {
                                                 <AccessTimeIcon className={classes.iconClass} />
                                                 <Typography className={classes.margin}>
                                                     {t[i]}
-                                                    </Typography>
+                                                </Typography>
                                             </Grid>
                                         </CardContent>
                                     </ButtonBase>
--- server/routes/student.js
+++ server/routes/student.js
@@ -3,11 +3,13 @@
 const express = require('express');
 const { CourseController } = require('../controllers/CourseController');
 const { StudentController } = require('../controllers/StudentController');
+const { ExamController } = require('../controllers/ExamController');
 
 const router = express.Router();
 
 router.get('/courses', CourseController.getCourses);
 router.get('/course', CourseController.getCourse);
 router.get('/get-image', StudentController.getStudentImage);
+router.get('/exam', ExamController.getExam);
 
 module.exports = router

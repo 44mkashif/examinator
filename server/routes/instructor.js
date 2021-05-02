b6c77c44903d6a21b46997d550388528a12d9eaf
Saif Hashmi <saifhashmi008@gmail.com>
2021-05-02 21:50:50 +0500

    ans,res schema added, APIs created

--- server/routes/instructor.js
+++ server/routes/instructor.js
@@ -4,6 +4,7 @@ const express = require('express');
 const { StudentController } = require('../controllers/StudentController');
 const { ExamController } = require('../controllers/ExamController');
 const { CourseController } = require('../controllers/CourseController');
+const { ResultController } = require('../controllers/ResultController');
 
 const router = express.Router();
 
@@ -24,5 +25,7 @@ router.get('/courses', CourseController.getCourses);
 router.get('/course', CourseController.getCourse);
 router.put('/course', CourseController.updateCourse);
 router.delete('/course', CourseController.deleteCourse);
+
+router.get('/results', ResultController.getResultForInstructor);
 
 module.exports = router

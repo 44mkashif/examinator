9db43edf175c6a13df3e046a027f906fe3d2a95d
Muhammad Kashif <44mkashif@gmail.com>
2021-05-07 18:55:21 +0500

    Student Result apis integrated

---
+++ client/src/services/ResultService.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+
+class ResultService extends Component {
+
+    static async getAnswers(examId, studentId, authToken) {
+        try {
+            const res = await axios.get(`http://localhost:4000/api/student//get-answers?examId=${examId}&studentId=${studentId}`, {
+                headers: {
+                    'auth-token': authToken
+                },
+            });
+            if (res.data.success) {
+                console.log(res.data);
+                return res.data.answers;
+            } else {
+                console.log(res.data.msg);
+            }
+        } catch (error) {
+            console.log(error.response)
+        }
+    }
+
+    static async addResult(body, authToken) {
+        try {
+            const res = await axios.post('http://localhost:4000/api/student/add-result', body, {
+                headers: {
+                    'auth-token': authToken
+                },
+            });
+            if (res.data.success) {
+                console.log(res.data);
+                return res.data;
+            }
+        } catch (error) {
+            console.log(error.response.data);
+            return error.response.data;
+        }
+    }
+
+    static async getResults(examId, studentId, authToken) {
+        try {
+            const res = await axios.get(`http://localhost:4000/api/student/result/?examId=${examId}&studentId=${studentId}`, {
+                headers: {
+                    'auth-token': authToken
+                },
+            });
+            if (res.data.success) {
+                console.log(res.data);
+                return res.data.result;
+            } else {
+                console.log(res.data.msg);
+            }
+        } catch (error) {
+            console.log(error.response)
+        }
+    }
+
+
+}
+
+export default ResultService;
